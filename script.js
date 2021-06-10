f1d0af7db487fcaf5553cff746d889ee5f3c0a65
Laura Gwen <lauraburch.lb@gmail.com>
2021-06-10 10:55:52 -0700

    added alerts to guide how many numbers needed for the password

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  var passwordLength = prompt("Select a password length between 8 and 128.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must choose a numer between 8 and 128");
+    return;
+  }
+  if (isNaN(passwordLength)) {
+    alert("Please choose a number.");
+    return;
+  }
 }
 
 // Add event listener to generate button
